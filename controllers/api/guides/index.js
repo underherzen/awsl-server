f0c48e0430f0978d143668a7e3232bc994803abf
JubasNTC <jubas1337@mail.ru>
2020-04-20 11:09:14 +0300

    add send url redirect when select guide

--- controllers/api/guides/index.js
+++ controllers/api/guides/index.js
@@ -1,20 +1,36 @@
-const {UserGuide, User, UserGuideDay, Guide, GuideDay} = require('../../../models');
-const moment = require('moment');
-const {Op} = require('sequelize');
+const { userToFront } = require("../../../modules/helpers");
+const moment = require("moment");
+const { Op } = require("sequelize");
+const { retrieveToken } = require("../../../modules/auth");
+const {
+  UserGuide,
+  User,
+  UserGuideDay,
+  Guide,
+  GuideDay,
+} = require("../../../models");
 
 const loadGuides = async (req, res, next) => {
   const guides = await Guide.findAll();
-  res.send(guides)
+  res.send(guides);
 };
 
-
 const selectGuide = async (req, res, next) => {
-  const body = req.body;
-  let user = req.user;
-  if (user.guide_id) {
-    res.status(400).send({error: 'You already have guide'});
+  const { body, headers } = req;
+  let { user } = req;
+
+  const token = await retrieveToken(headers);
+
+  if (!token) {
+    res.sendStatus(401);
     return;
   }
+
+  if (user.guide_id) {
+    res.status(400).send({ error: "You already have guide" });
+    return;
+  }
+
   const guide = await Guide.findByPk(body.guide_id);
 
   if (!guide) {
@@ -22,17 +38,15 @@ const selectGuide = async (req, res, next) => {
     return;
   }
 
-  const previousSameGuide = await UserGuide.findOne(
-    {
-      where: {
-        user_id: user.id,
-        guide_id: guide.id
-      }
-    }
-  );
+  const previousSameGuide = await UserGuide.findOne({
+    where: {
+      user_id: user.id,
+      guide_id: guide.id,
+    },
+  });
 
   if (previousSameGuide) {
-    res.status(400, {error: 'You have already passed this guide!'});
+    res.status(400, { error: "You have already passed this guide!" });
     return;
   }
 
@@ -41,32 +55,32 @@ const selectGuide = async (req, res, next) => {
     UserGuide.create({
       user_id: user.id,
       guide_id: guide.id,
-      day: dayToAssign
+      day: dayToAssign,
     }),
     UserGuideDay.create({
       user_id: user.id,
       guide_id: guide.id,
-      day: dayToAssign
+      day: dayToAssign,
     }),
-    User.update(
-      {guide_id: guide.id},
-      {where: {id: user.id}}
-    )
+    User.update({ guide_id: guide.id }, { where: { id: user.id } }),
   ];
+
   if (user.is_active) {
     promises.push(
       UserGuideDay.create({
         user_id: user.id,
         guide_id: guide.id,
-        day: 0
-      }))
+        day: 0,
+      })
+    );
   }
   // TODO: MAKE SENDING TEXTS
   await Promise.all(promises);
-  user = await User.findByPk(user.id);
-  res.send({user});
 
+  user = await userToFront(token.user_id);
+  const redirect = `/guides/${guide.url_safe_name}/intro/`;
 
+  res.send({ user, redirect });
   // res.sendStatus(400);
 };
 
@@ -75,42 +89,44 @@ const getGuideDay = async (req, res, next) => {
   const guideDay = await GuideDay.findOne({
     where: {
       guide_id: body.guide_id,
-      day: +body.day
-    }
+      day: +body.day,
+    },
   });
   if (!guideDay) {
     res.sendStatus(404);
     return;
   }
 
-  res.send({guide_day: guideDay});
+  res.send({ guide_day: guideDay });
 };
 
 const getGuideDaysForSlider = async (req, res, next) => {
   const body = req.query;
   if (!body.guide_id) {
     res.sendStatus(400);
-    return
+    return;
   }
   const guideDays = await GuideDay.findAll({
-    attributes: ['title', 'day'],
+    attributes: ["title", "day"],
     where: {
       guide_id: body.guide_id,
       day: {
-        [Op.lt]: 22
-      }
-    }
+        [Op.lt]: 22,
+      },
+    },
   });
+
   if (!guideDays) {
     res.sendStatus(404);
-    return
+    return;
   }
-  res.send({guide_days: guideDays})
+
+  res.send({ guide_days: guideDays });
 };
 
 module.exports = {
   selectGuide,
   getGuideDay,
   getGuideDaysForSlider,
-  loadGuides
+  loadGuides,
 };
