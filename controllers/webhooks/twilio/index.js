80ffcef9bbea497af67fe1d950a0bffdbc01d7de
JubasNTC <jubas1337@mail.ru>
2020-05-05 00:00:08 +0300

    fix card

--- controllers/webhooks/twilio/index.js
+++ controllers/webhooks/twilio/index.js
@@ -36,9 +36,9 @@ const replyWebhook = async (req, res, next) => {
 
     const body = req.body;
     let sentCommand = body.Body.trim().toUpperCase();
-    // if (sentCommand.indexOf(' ') > -1) {
-    //   sentCommand = sentCommand.split(' ')[0]
-    // }
+    if (sentCommand.indexOf(' ') > -1) {
+      sentCommand = sentCommand.split(' ')[0];
+    }
     let messageBody;
 
     const authCommands = [REPLY_COMMANDS.YES, REPLY_COMMANDS.HELP, REPLY_COMMANDS.STOP, REPLY_COMMANDS.UNSTOP];
@@ -111,7 +111,8 @@ const replyWebhook = async (req, res, next) => {
         });
       }
       message.body(messageBody);
-      res.send(twiml.toString());
+      res.writeHead(200, { 'Content-Disposition': 'inline' });
+      res.end(twiml.toString());
       return;
     } else if (sentCommand === REPLY_COMMANDS.COMMUNITY) {
       messageBody = REPLY_TEXTS.COMMUNITY;
