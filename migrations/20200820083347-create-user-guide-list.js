be6a39a795b7138975c61f5f3d43d9ade7219d08
JubasNTC <jubas1337@mail.ru>
2020-08-20 13:12:14 +0300

    create migrations for new onboarding

---
+++ migrations/20200820083347-create-user-guide-list.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('user_guides_lists', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('user_guides_lists');
+  }
+};
---
+++ models/user_guide_list.js
@@ -0,0 +1,27 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const UserGuideList = sequelize.define(
+    'UserGuideList',
+    {
+      user_id: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      completed: { type: DataTypes.BOOLEAN, defaultValue: false },
+    },
+    {
+      tableName: 'user_guides_lists',
+      timestamps: true,
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    }
+  );
+  UserGuideList.associate = function (models) {
+    // associations can be defined here
+  };
+  return UserGuideList;
+};
