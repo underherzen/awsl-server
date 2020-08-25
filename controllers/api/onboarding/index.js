8d0f178dd6a6c7eb5a2d874f28e26e57359f2722
JubasNTC <jubas1337@mail.ru>
2020-08-25 17:49:00 +0300

    change onboarding route

--- controllers/api/onboarding/index.js
+++ controllers/api/onboarding/index.js
@@ -1,45 +1,45 @@
 const { Guide, User, UserGuide, UserGuideList } = require('../../../models');
 
 const passOnboarding = async (req, res, next) => {
-  const {
-    user: { id },
-    body: { onboardingData, selectedGuides },
-  } = req;
+  try {
+    const {
+      user: { id },
+      body: { onboardingData, selectedShortNames },
+    } = req;
 
-  /*   try {
     await User.update(onboardingData, { where: { id } });
-  } catch (error) {
-    return res.status(500);
-  }  */
 
-  /*   const guides = await Guide.findAll({
-    attributes: ['short_name', 'id'],
-    where: {
-      is_active: true,
-    },
-    order: [['position', 'ASC']],
-  }).map((guide) => Object.values(guide.dataValues));
+    const actualGuides = await Guide.findAll({
+      attributes: ['short_name', 'id'],
+      where: {
+        is_active: true,
+      },
+      order: [['position', 'ASC']],
+    }).map((guide) => Object.values(guide.dataValues));
 
-  const guidesMapping = new Map(guides);
-  const shortNames = guidesMapping.keys();
+    const guidesMapping = new Map(actualGuides);
+    const shortNames = guidesMapping.keys();
 
-  const formedShortNames = new Set([...selectedGuides, ...shortNames]);
-  const userGuidesShortNames = Array.from(formedShortNames);
+    const formedShortNames = new Set([...selectedShortNames, ...shortNames]);
+    const convertedShortNames = Array.from(formedShortNames);
 
-  const userGuidesList = await UserGuideList.create({ user_id: id });
+    const userGuidesList = await UserGuideList.create({ user_id: id });
 
-  const formedUserGuides = uniqueShortNames.map((short_name, index) => {
-    return {
-      guides_list_id: userGuidesList.dataValues.id,
-      guide_id: guidesMapping.get(short_name),
-      position: ++index,
-      user_id: id,
-    };
-  });
+    const formedUserGuides = convertedShortNames.map((short_name, index) => {
+      return {
+        guides_list_id: userGuidesList.dataValues.id,
+        guide_id: guidesMapping.get(short_name),
+        position: ++index,
+        user_id: id,
+      };
+    });
 
-  const result = await UserGuide.bulkCreate(formedUserGuides);
+    await UserGuide.bulkCreate(formedUserGuides);
 
-  return res.status(200).send(result); */
+    return res.status(200);
+  } catch (e) {
+    next(e);
+  }
 };
 
 module.exports = {
